b6c75f4590d8b7674944c7b96a78056649f0995f
Daniele <danielepalombi@protonmail.com>
2022-07-20 01:40:11 +0200

    antialiasing

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ mod ray;
 mod hittable;
 mod hittable_list;
 mod sphere;
+mod camera;
 
 use std::f64::INFINITY;
+use rand::prelude::*;
 
 use vec3::{Vec3, Point3};
 use color::{Color, write_color};
@@ -14,6 +16,7 @@ use hittable::Hit_Record;
 use hittable::Hittable;
 use hittable_list::Hittable_List;
 use sphere::Sphere;
+use camera::*;
 
 fn ray_color(r: Ray, world: &Box<dyn Hittable>) -> Color {
     let mut rec : Hit_Record = Hit_Record::default();
@@ -47,27 +50,20 @@ fn main() {
     let aspect_ratio: f64 = 16.0 / 9.0;
     let image_width: i32 = 400;
     let image_height: i32 = ((image_width as f64) / aspect_ratio) as i32;
+    let samples_per_pixel: i32 = 100;
 
     // World
-
-    // Vec<Box<dyn Hittable>>
-    let mut object_list : Vec<Box<dyn Hittable>> = Vec::new();
-
-    object_list.push(Box::new(Sphere::new(Point3::new(0.0, -100.0, -1.0), 100.0)));    
+    let mut object_list : Vec<Box<dyn Hittable>> = Vec::new();   
     object_list.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    object_list.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0))); 
 
-    
     let world : Box<dyn Hittable> = Box::new(Hittable_List::new(object_list));
 
     // Camera
-    let viewport_height: f64 = 2.0;
-    let viewport_width: f64 = aspect_ratio * viewport_height;
-    let focal_length: f64 = 1.0;
+    let cam = Camera::default();
 
-    let origin: Point3 = Point3::new(0.0, 0.0, 0.0);
-    let horizontal: Vec3 = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical: Vec3 = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner: Point3 = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    // Initialize rng?
+    let mut rng = rand::thread_rng();
 
     //Render
     println!("P3");
@@ -77,44 +73,19 @@ fn main() {
     for i in (0..image_height).rev() {
         eprintln!("Scanlines remaining: {} " , i);
         for j in 0..image_width {
-            let u: f64 = (j as f64) / ((image_width - 1) as f64);
-            let v: f64 = (i as f64) / ((image_height - 1) as f64);
-            let r: Ray = Ray::ray(origin, lower_left_corner + horizontal * u + vertical * v - origin);
-            let pixel_color: Color = ray_color(r, &world);
+            let mut pixel_color: Color = Color::new(0.0, 0.0, 0.0);
+            for s in 0..samples_per_pixel {
+                let rand1: f64 = rng.gen();
+                let rand2: f64 = rng.gen();
+                let u: f64 = ((j as f64) + rand1) / ((image_width - 1) as f64);
+                let v: f64 = ((i as f64) + rand2) / ((image_height - 1) as f64);
 
-            write_color(pixel_color)
+                let r: Ray = cam.get_ray(u, v);
+                pixel_color = pixel_color + ray_color(r, &world);
+            }
+            write_color(pixel_color, samples_per_pixel);
         }
     }
 
     eprintln!("Done!");
 }
-
-/*
-fn main() {
-    // Image
-    let image_width: i32 = 256;
-    let image_height: i32 = 256;
-
-    // Render
-    println!("P3");
-    println!("{} {}" , image_width , image_height);
-    println!("255");
-
-    for i in (0..image_height).rev() {
-        eprintln!("Scanlines remaining: {} " , i);
-        for j in 0..image_width {
-            let r: f64 = (j as f64) / ((image_width as f64) - 1.0);
-            let g: f64 = (i as f64) / ((image_height as f64) - 1.0);
-            let b: f64 = 0.25;
-
-            let ir: i32 = (255.999 * r) as i32;
-            let ig: i32 = (255.999 * g) as i32;
-            let ib: i32 = (255.999 * b) as i32;
-
-            println!("{} {} {}" , ir , ig , ib);
-        }
-    }
-
-    eprintln!("Done!");
-}
-*/
